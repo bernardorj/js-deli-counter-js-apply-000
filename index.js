e75aaeaa2546f13d442437353da333950483f0cc
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-28 21:19:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function takeANumber(katzDeliLine, name){
-  for (let i=0;; i++)
-  let positionInLine = i+1; //0 index is 1st person in line
+  for (let i=0;; i++) {
+    let positionInLine = i+1; //0 index is 1st person in line
 
-  katzDeliLine = katzDeliLine.push(name);
+    katzDeliLine = katzDeliLine.push(name);
 
-  return `Welcome, ${name}. You are number ${positionInLine} in line.`
+    return `Welcome, ${name}. You are number ${positionInLine} in line.`  
+  }
 }
