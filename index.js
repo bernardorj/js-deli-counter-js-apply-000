9acc56bb63708a1cfe48d197f1a082ec3361de5b
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-28 23:36:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(deliLine){
-if (deliLine !== undefined){
+if (deliLine.length !== 0){
   return `Currently serving ${deliLine[0]}.`;  //first person in line has index = 0
   deliLine.shift();
 } else {
