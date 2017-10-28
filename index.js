2e6155f4867950ccb8c4f1c435d9290af12a3aec
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-28 23:48:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,5 +16,10 @@ function nowServing(deliLine){
 }
 
 function currentLine(line){
-  
+  if (line.length !== 0){
+    return `The line is currently: ${line.}`
+    
+  } else {
+    return "The line is currently empty."
+  }
 }
