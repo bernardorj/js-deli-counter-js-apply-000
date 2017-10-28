35bed66140d2f70578115a952d12bbfac6588c3e
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-28 21:33:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 function takeANumber(katzDeliLine, name){
   for (let i=0;; i++) {
-    if (i===0){
       let positionInLine = i+1; //0 index is 1st person in line
 
       katzDeliLine = katzDeliLine.push(name);
 
       return `Welcome, ${name}. You are number ${positionInLine} in line.`
-    } else {
-      positionInLine = i+1; //0 index is 1st person in line
-
-      katzDeliLine = katzDeliLine.push(name);
-
-      return `Welcome, ${name}. You are number ${positionInLine} in line.`
-    }
   }
 }
