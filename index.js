c4a2b18f137a1447fc7e1c2f21bf34e1645369c1
Rachel Bernardo <racheljbernardo@gmail.com>
2017-10-28 22:45:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ function takeANumber(katzDeliLine, name){
     return `Welcome, ${name}. You are number ${positionInLine} in line.`
 }
 
-function nowServing(katzDeliLine){
-    if (katzDeliLine.length > 0) {
-      return `Currently serving ${katzDeliLine[0]}.` //first person in line has index = 0
+function nowServing(deliLine){
+    if (deliLine.length > 0) {
+      return `Currently serving ${deliLine[0]}.` //first person in line has index = 0
     } else {
       return "There is nobody waiting to be served!"
   }
